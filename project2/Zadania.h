85ff591936d3f79044ccd3851a4d1f94062a3000
Bartłomiej Nawój <thebalowen@gmail.com>
2020-06-07 23:31:22 +0200

    add switch for mieszadelko (swizzle stick xD)

--- project2/Zadania.h
+++ project2/Zadania.h
@@ -2,8 +2,8 @@
 // Bart³omiej Nawój -			Program testowy symuluj¹cy produkcje napoju		   //
 /////////////////////////////////////////////////////////////////////////////////////
 bool auto_mode=1;
-int tm1 = 0, tm2a=0, tm2b=0, tm3=0, wybrany_zawor = 0;
-char stan1=1, stan2=1, stan3=0, stan_manual=0;
+int tm1 = 0, tm2a=0, tm2b=0, tm3=0, tm4=0,  wybrany_zawor = 0;
+char stan1=1, stan2=1, stan3=0, stan_manual=0, stan_mieszadelko=1;
 
 // Program testowy symuluj¹cy produkcje napoju
 void test_A(void)
@@ -97,7 +97,34 @@ void test_A(void)
 					break;
 			}	
 		if(tm3) --tm3;
-	
+
+		switch(stan_mieszadelko){
+			// switch dla ukladu sterujacego mieszadelkiem w trybie auto
+			case 1: M=0;
+				if(stan3==4) {tm4 = 4; stan_mieszadelko=2;}
+				break;
+			case 2: M=1;
+				if(!tm4) {tm4 = 4; stan_mieszadelko=3;}
+				else if(stan3==6) stan_mieszadelko=4;
+				break;
+			case 3: M=0;
+				if(!tm4) {tm4 = 4; stan_mieszadelko=2;}
+				else if(stan3==6) stan_mieszadelko=4;
+				break;
+			case 4: M=1;
+				if(stan3==7) {tm4=8; stan_mieszadelko=5;}
+				break;
+			case 5: M=1;
+				if(!tm4) {tm4=6; stan_mieszadelko = 6;}
+				else if(stan3==1) stan_mieszadelko=1;
+				break;
+			case 6: M=0;
+				if(!tm4) {tm4=8; stan_mieszadelko = 5;}
+				else if(stan3==1) stan_mieszadelko = 1;
+				break;
+		}
+		if(tm4) --tm4;
+
 		sprintf(buf,"K1=%dK2=%dK3=%dK4=%d ",(int)aK1,(int)aK2,(int)aK3,(int)aK4);
 		LCD_xy(1,1); LCD_puts(buf);
 		sprintf(buf,"L1=%dL2=%dL3=%dL4=%d ",(int)L1,(int)L2,(int)L3,(int)L4);
@@ -107,13 +134,6 @@ void test_A(void)
 		// tryb manualny		
 		//automode = false
 
-		//Z1 = aK1 ? 1 : 0;				// Otwórz Z1 jeœli naciœniêty K1
-		//Z2 = aK2 ? 1 : 0;				// Otwórz Z2 jeœli naciœniêty K2
-		//Z3 = aK3 ? 1 : 0;				// Otwórz Z3 jeœli naciœniêty K3
-		//Z4 = aK4 ? 1 : 0;				// Otwórz Z4 jeœli naciœniêty K4
-		//Z5 = aK5 ? 1 : 0;				// Otwórz Z5
-		//M  = 0x1 ? 1 : 0;				// W³¹cz  M (mieszanie)
-		//G  = aK6 ? 1 : 0;				// Wy³¹cz G (grzanie)
 		switch(stan_manual){
 			case 0: // oczekiwanie i zliczanie wyboru zaworu (aK1)
 				Z1=0; Z2=0; Z3=0; Z4=0; Z5=0; G=0; M=0;
@@ -123,19 +143,20 @@ void test_A(void)
 				}
 				if(aK2) stan_manual=1;	// trzymanie aK2 w³¹cza wybrany zawór (G i M równie¿)
 				break;
-			case 1: wybrany_zawor == 0 ? Z1=1 : 0;
-					wybrany_zawor == 1 ? Z2=1 : 0;
-					wybrany_zawor == 2 ? Z3=1 : 0;
-					wybrany_zawor == 3 ? Z4=1 : 0;
-					wybrany_zawor == 4 ? Z5=1 : 0;
-					wybrany_zawor == 5 ? M=1 : 0;
-					wybrany_zawor == 6 ? G=1 : 0;
+			case 1: wybrany_zawor == 0 ? Z1=1 : 0;	// Otwórz Z1 jeœli wybrany_zawor == 0
+					wybrany_zawor == 1 ? Z2=1 : 0;	// Otwórz Z2 jeœli wybrany_zawor == 1
+					wybrany_zawor == 2 ? Z3=1 : 0;	// Otwórz Z3 jeœli wybrany_zawor == 2
+					wybrany_zawor == 3 ? Z4=1 : 0;	// Otwórz Z4 jeœli wybrany_zawor == 3
+					wybrany_zawor == 4 ? Z5=1 : 0;	// Otwórz Z5 jeœli wybrany_zawor == 4
+					wybrany_zawor == 5 ? M=1 : 0;	// W³¹cz M jeœli wybrany_zawor == 5
+					wybrany_zawor == 6 ? G=1 : 0;	// W³¹cz G jeœli wybrany_zawor == 6
 
-					if(!aK2) stan_manual=0;
+					if(!aK2) stan_manual=0;			// wróæ do stanu 0 jeœli puszczono aK2
 					break;
 			}
 		
-		sprintf(buf,"wybrany zawor=%d ",(int)wybrany_zawor);
+		// LCD, wyœwietl wybrany_zawor+1 dla czytelnoœci / Z1 to wtedy zawor 1, a nie 0
+		sprintf(buf,"wybrany zawor=%d ",((int)wybrany_zawor+1));	
 		LCD_xy(1,1); LCD_puts(buf);
 		sprintf(buf,"L1=%dL2=%dL3=%dL4=%d ",(int)L1,(int)L2,(int)L3,(int)L4);
 		LCD_xy(1,2); LCD_puts(buf);
